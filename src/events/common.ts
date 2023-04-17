5790bf86ad38da402e6ba3512647ad214ed5a42a
Marco Chavez <43889446+mxrcochxvez@users.noreply.github.com>
2023-04-17 10:56:05 -0700

    feat(onMessageDelete): implements event

--- src/events/common.ts
+++ src/events/common.ts
@@ -1,10 +1,70 @@
+import {
+  APIEmbedField,
+  ChannelType,
+  EmbedAuthorOptions,
+  EmbedBuilder,
+  TextChannel,
+} from "discord.js";
 import type { ArgsOf, Client } from "discordx";
 import { Discord, On } from "discordx";
 
 @Discord()
 export class Example {
+  channelName: string = "logs";
+
   @On()
   messageDelete([message]: ArgsOf<"messageDelete">, client: Client): void {
-    console.log("Message Deleted", client.user?.username, message.content);
+    const cachedChannels = client.channels.cache as unknown as TextChannel[];
+    const logChannel = cachedChannels.find((c) => c.name === this.channelName);
+    const currentGuild = client.guilds.cache.get(message.guild?.id || "");
+
+    const deletedEmbed = (
+      author: EmbedAuthorOptions,
+      message: APIEmbedField,
+      channel: APIEmbedField
+    ) =>
+      new EmbedBuilder()
+        .setColor("DarkPurple")
+        .setAuthor(author)
+        .addFields(message)
+        .addFields(channel)
+        .setTimestamp();
+
+    if (logChannel !== undefined)
+      logChannel.send({
+        embeds: [
+          deletedEmbed(
+            { name: `Author: ${message.author?.username}` },
+            { name: "Message Content", value: message.content || "" },
+            {
+              name: "From Channel",
+              value: `${(message.channel as TextChannel).name}`,
+            }
+          ),
+        ],
+      });
+    else {
+      currentGuild?.channels
+        .create({
+          name: this.channelName,
+          type: ChannelType.GuildText,
+        })
+        .then((fulfilled) => {
+          console.log(fulfilled);
+          fulfilled.send({
+            embeds: [
+              deletedEmbed(
+                { name: `Author: ${message.author?.username}` },
+                { name: "Message Content", value: message.content || "" },
+                {
+                  name: "From Channel",
+                  value: `${(message.channel as TextChannel).name}`,
+                }
+              ),
+            ],
+          });
+        })
+        .catch((err) => console.error(err));
+    }
   }
 }
